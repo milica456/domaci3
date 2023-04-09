913f64a2a792bb728addd4df5a8882bd2dba14dd
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-04-09 10:19:09 +0200

    Kreiran Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/navbar';
+import {Home} from "./pages/home/home";
+import {Shop} from "./pages/shop/shop";
+import {Cart} from "./pages/cart/cart";
 
 function App() {
   return (
-    <div className="App">
-     
+   <div className="App"> 
+    
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />}/>
+        <Route path="/cart" element={<Cart />} />
+
+      </Routes>
+    </Router>
     </div>
   );
 }
