7c6899df39b0ff6dbf276c18071c3ac2ddfe79de
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-04-10 11:51:28 +0200

    Kreirana lista proizvoda

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productNme: "Pepperoni Pizza",
+        price: 890.00,
+        priductImage: product1, 
+    },
+    {
+        id: 2,
+        productNme: "Margarita Pizza",
+        price: 850.00,
+        priductImage: product2,
+    },
+    {
+        id: 3,
+        productNme: "Special Pizza",
+        price: 1200.00,
+        priductImage: product3,
+    },
+    {
+        id: 4,
+        productNme: "Vegan Pizza",
+        price: 800.00,
+        priductImage: product4,
+    },
+    {
+        id: 5,
+        productNme: "Pineapple Pizza",
+        price: 1000.00,
+        priductImage: product5,
+    },
+    {
+        id: 6,
+        productNme: "Mix Pizza",
+        price: 1300.00,
+        priductImage: product6,
+    }
+]
