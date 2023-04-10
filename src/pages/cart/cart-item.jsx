7939b794a9003473b1412939c92267aca59d0f72
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-04-10 23:06:07 +0200

    dugme u korpi

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { MenuContext } from '../../context/menu-context';
 
 export const CartItem = (props) => {
 const {id, productName, price, productImage} = props.data;
-
+const {cartItems } = useContext(MenuContext);
   return ( 
   <div className='cartItem'> 
   <div className='cartItemImage' style={{ backgroundImage: `url(${productImage})` }}> </div>
  
   <h1> {productName}</h1>
   <p> {price}rsd</p>
+  <div className='countHandler'>
+    <button> + </button>
+    <input value={cartItems[id]}/>
+    <button> - </button>
+  </div>
   </div>
 
   );
