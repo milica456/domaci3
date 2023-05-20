4a757e551ef8326ecdf57d9c033469e0d3de1fb9
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-05-20 19:58:43 +0200

    filteri za pretragu

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
 import "./menu.css";
 
+// Pretraga proizvoda po ceni
 export const Menu = () => {
- 
-  return (
-    
-    <div className='menu'>
-      <h1 className='menuTitle'> Menu </h1> 
-      
-      
-      <div className='products'> 
-      {PRODUCTS.map((product) => (
-      <Product data={product}/>
-      ))}
+  const [filters, setFilters] = useState({});
 
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
+  };
+
+  const applyFilters = (product) => {
+    const { price } = filters;
+
+    // Vraca proizvode sa istom ili manjom cenom
+    if (price && product.price > parseInt(price)) {
+      return false;
+    }
+
+   
+    return true;
+  };
+
+  const filteredProducts = PRODUCTS.filter(applyFilters);
+
+  return (
+    <div className='menu'>
+      <h1 className='menuTitle'>Menu</h1>
+      <div className='filters'>
+        <label htmlFor='price'>Cena:</label>
+        <input
+          type='number'
+          id='price'
+          name='price'
+          value={filters.price || ''}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <div className='products'>
+        {filteredProducts.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
       </div>
     </div>
-    
   );
 };
-
